f5cbc83c71140cae72eb7706c155143c09e3e327
JRH89 <thegemlins@gmail.com>
2023-08-02 20:50:22 -0700

    ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,9 +29,7 @@ const Navbar = () => {
 	return (
 		<nav className="fixed top-0 left-0 w-full z-50 bg-white shadow">
 			{/* Add the fixed scrollbar container with gradient background */}
-
-
-			<div className="container flex justify-center items-center text-center mx-auto py-2">
+			<div className="container flex justify-center items-center text-center mx-auto pb-2 pt-1">
 				<ul className="flex text-black flex-row items-center space-x-6">
 					<li className="hover:scale-95 cursor-pointer duration-200">
 						<Link to="hero" smooth={true} duration={700}>
