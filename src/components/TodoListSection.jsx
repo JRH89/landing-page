6e371c2f23189e08829568562422b84fb39ee4df
JRH89 <thegemlins@gmail.com>
2023-08-03 21:21:50 -0700

    ui

--- src/components/TodoListSection.jsx
+++ src/components/TodoListSection.jsx
@@ -7,7 +7,7 @@ const TodoListSection = () => {
 				<h1 className='text-3xl'><i className="fa-solid fa-list-ol rounded-lg text-red-400"></i></h1>
 				<h2 className="text-3xl font-bold mb-5">ToDo List</h2>
 
-				<div className='flex flex-col gap-8 justify-center items-center'>
+				<div className='flex flex-col gap-5 justify-center items-center'>
 					<p className="">
 						Completed a task? Congratulations! You can instantly delete a task with a single click or tap.
 						Feel a sense of accomplishment as you tick off completed tasks from your list.
@@ -23,7 +23,7 @@ const TodoListSection = () => {
 						Stay organized, efficient, and focused on what matters. Let our Todo List be your digital assistant
 						in managing your daily tasks and achieving your goals.
 					</p>
-					<div className="grid mx-5 sm:mx-0 gap-4 grid-cols-1 sm:grid-cols-2 mb-5">
+					<div className="grid mx-5 sm:mx-0 gap-4 grid-cols-1 sm:grid-cols-2 mt-6">
 						<div className="bg-gray-800 rounded-lg p-6">
 							<p>
 								With our app&apos;s built-in Todo List feature, you can manage and organize your tasks effectively.
